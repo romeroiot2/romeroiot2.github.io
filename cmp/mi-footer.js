b71cc41b182677b6a10a47a0a8f2c091638b2260
romeroiot2 <82291962+romeroiot2@users.noreply.github.com>
2021-04-13 18:48:03 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `&copy; 2021
-      Gilberto Pacheco Gallegos.`;
+      RomeroVillegasJesúsAntonio.`;
   }
 }
 customElements.define(
